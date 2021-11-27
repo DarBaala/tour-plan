fa8b77d939083db61018ae10ecba0bd51ef9247d
DarBaala <joker.milan1997gmail.com>
2021-11-27 21:46:44 +0300

    c

--- js/main.js
+++ js/main.js
@@ -7,4 +7,8 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".slider-button--next",
     prevEl: ".slider-button--prev",
   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 });
