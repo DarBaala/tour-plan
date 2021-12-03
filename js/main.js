612efe62a12d0f7e3285144224664d800c17777f
DarBaala <joker.milan1997gmail.com>
2021-12-03 16:49:25 +0300

    Create

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-const swiper = new Swiper(".swiper", {
+const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
   keyboard: {
     enabled: true,
