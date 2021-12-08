4d1301fe8d8af4434af74e4bdf40763c60769108
DarBaala <joker.milan1997gmail.com>
2021-12-08 13:47:08 +0300

    Create: added mobile menu

--- js/main.js
+++ js/main.js
@@ -26,3 +26,11 @@ const reviewsSlider = new Swiper(".reviews-slider", {
     onlyInViewport: false,
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
